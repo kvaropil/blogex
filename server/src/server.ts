7abce8f5376a86a8e7024a1767af078790daac63
Roman Kvapil <kvapilrom@gmail.com>
2023-07-15 01:51:01 +0200

    chore: Add cors and @types/cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/v1/posts', (req, res) => {
+interface IPost {
+  id: number;
+  title: string;
+  content: string;
+}
+
+app.use(cors());
+
+app.get('/api/v1/posts', (req: Request, res: Response<IPost[]>) => {
   res.json([
     { id: 1, title: 'First post', content: 'Hello, this is my first post' },
     { id: 2, title: 'Second post', content: 'Here is some more content' },
