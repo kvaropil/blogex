dd930f2dd6ea7e74beeb1ea2dba180ece78a70ca
Roman Kvapil <kvapilrom@gmail.com>
2023-07-15 01:50:23 +0200

    feat: Add sass and basic styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+import styles from './App.module.scss';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+
+const App: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const response: AxiosResponse<Post[]> = await axios.get<Post[]>(
+          'http://localhost:3000/api/v1/posts',
+        );
+        setPosts(response.data);
+      } catch (err) {
+        const axiosError = err as AxiosError;
+        console.error(axiosError.message);
+      }
+    };
+
+    void fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.app}>
+      <h1>Blogex</h1>
+      {posts.map((post) => (
+        <div key={post.id} className={styles.post}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
